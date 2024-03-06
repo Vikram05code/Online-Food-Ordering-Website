e8056446bdd3d678c55b6235fa8909b47690c9c1
Vikram <vikky5vikram@gmail.com>
2024-03-06 17:05:16 +0530

    Initial Commit for Online Food Ordering Website

---
+++ src/main/java/com/vikram/Online_Food_Ordering_Website/OnlineFoodOrderingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.vikram.Online_Food_Ordering_Website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderingWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikram/Online_Food_Ordering_Website/OnlineFoodOrderingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.Online_Food_Ordering_Website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
