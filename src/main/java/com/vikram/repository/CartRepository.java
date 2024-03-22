fdf08bdc365b58ddf154a26594a9603c5cbb27df
Vikram <vikky5vikram@gmail.com>
2024-03-22 15:47:32 +0530

    Committing code for CartRepository

---
+++ src/main/java/com/vikram/repository/CartRepository.java
@@ -0,0 +1,12 @@
+package com.vikram.repository;
+
+import com.vikram.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+   Optional<Cart> findByCustomer_Id(Long userId);
+
+}
