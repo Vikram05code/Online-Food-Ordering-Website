940381c5d554bf75975355e1a32cf43aa5608a44
Vikram <vikky5vikram@gmail.com>
2024-03-22 16:20:14 +0530

    Committing code for CategoryRepository

---
+++ src/main/java/com/vikram/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.vikram.repository;
+
+import com.vikram.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    public List<Category> findByRestaurantId(Long id);
+
+}
