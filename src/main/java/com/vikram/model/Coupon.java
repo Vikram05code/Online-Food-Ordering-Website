7c5b26d8261664814fda4e78595519b5b2453435
Vikram <vikky5vikram@gmail.com>
2024-03-22 18:19:16 +0530

    Committing code for Coupon Model class

---
+++ src/main/java/com/vikram/model/Coupon.java
@@ -0,0 +1,28 @@
+package com.vikram.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+    private double discountAmount;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date validityPeriod;
+
+    private String termsAndConditions;
+
+}
