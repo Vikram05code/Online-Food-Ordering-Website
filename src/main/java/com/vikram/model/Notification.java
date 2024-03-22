e6f5219e44ca43d822f12d47667d3e1d015560a9
Vikram <vikky5vikram@gmail.com>
2024-03-22 17:10:07 +0530

    Committing code for Notification Model class

---
+++ src/main/java/com/vikram/model/Notification.java
@@ -0,0 +1,35 @@
+package com.vikram.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @ManyToOne
+   @JoinColumn(name="recipient_id")
+   private User customer;
+
+   @ManyToOne
+   private Restaurant restaurant;
+
+   private String message;
+
+   @Temporal(TemporalType.TIMESTAMP)
+   private Date sentAt;
+
+   private boolean readStatus;
+
+
+}
