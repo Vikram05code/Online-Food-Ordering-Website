9c3323b6ca38f54c75129d52bfd26c1ce5979c76
Vikram <vikky5vikram@gmail.com>
2024-03-24 01:00:19 +0530

    Committing code for AddCartItemRequest

---
+++ src/main/java/com/vikram/request/AddCartItemRequest.java
@@ -0,0 +1,11 @@
+package com.vikram.request;
+
+import java.util.List;
+
+public class AddCartItemRequest {
+
+    private Long menuItemId;
+    private int quantity;
+    private List<String> ingredients;
+
+}
