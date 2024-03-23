34422e1701e68747928bac57d3e89168a535da9e
Vikram <vikky5vikram@gmail.com>
2024-03-23 23:53:04 +0530

    Committing code for FoodRepository

---
+++ src/main/java/com/vikram/repository/FoodRepository.java
@@ -0,0 +1,21 @@
+package com.vikram.repository;
+
+import com.vikram.model.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface FoodRepository extends JpaRepository<Food, Long> {
+
+    List<Food> findByRestaurntId(Long restaurantId);
+
+    @Query("SELECT f FROM Food f WHERE "
+            + "f.name LIKE %:keyword% OR "
+            + "f.foodCategory.name LIKE %:keyword% AND "
+            + "f.restaurant!=null"
+    )
+    List<Food> searchByNameOrCategory(@Param("keyword") String keyword);
+
+}
