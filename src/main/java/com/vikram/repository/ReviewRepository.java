7a29a5b639a96cfc2017422796404bff4ddf82e6
Vikram <vikky5vikram@gmail.com>
2024-03-24 00:23:11 +0530

    Committing code for ReviewRepository and Updating Code for UserRepository

---
+++ src/main/java/com/vikram/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.vikram.repository;
+
+import com.vikram.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+
+
+}
--- src/main/java/com/vikram/repository/UserRepository.java
+++ src/main/java/com/vikram/repository/UserRepository.java
@@ -2,8 +2,15 @@ package com.vikram.repository;
 
 import com.vikram.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
+   @Query("SELECT u FROM User u Where u.status='PENDING'")
+   public List<User> getPenddingRestaurantOwners();
 
    public User findByEmail(String userName);
 
