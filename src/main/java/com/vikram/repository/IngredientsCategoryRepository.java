97bac5cc3816f5e63ebdc10ebcaa8696b65faeb7
Vikram <vikky5vikram@gmail.com>
2024-03-23 23:55:53 +0530

    Committing code for IngredientsCategoryRepository

---
+++ src/main/java/com/vikram/repository/IngredientsCategoryRepository.java
@@ -0,0 +1,22 @@
+package com.vikram.repository;
+
+import com.vikram.model.IngredientCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface IngredientsCategoryRepository extends JpaRepository<IngredientCategory, Long> {
+
+    //	List<IngredientCategory> findByFoodId(Long menuItemId);
+    List<IngredientCategory> findByRestaurantId(Long id);
+
+    @Query("SELECT e FROM IngredientCategory e "
+            + "WHERE e.restaurant.id = :restaurantId "
+            + "AND lower(e.name) = lower(:name)")
+    IngredientCategory findByRestaurantIdAndNameIgnoreCase(
+            @Param("restaurantId") Long restaurantId, @Param("name") String name);
+
+
+}
