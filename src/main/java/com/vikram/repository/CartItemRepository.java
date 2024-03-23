34347b3bc54bcfbf9e5f7ec47a7dce47753a9ceb
Vikram <vikky5vikram@gmail.com>
2024-03-23 23:47:30 +0530

    Committing code for CartItemRepository

--- src/main/java/com/vikram/repository/CartItemRepository.java
+++ src/main/java/com/vikram/repository/CartItemRepository.java
@@ -1,1 +1,10 @@
+package com.vikram.repository;
 
+import com.vikram.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+//    CartItem findByFoodIsContaining
+
+}
