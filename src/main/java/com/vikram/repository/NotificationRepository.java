34b2ad852c63b72b93fab5a151edda5cb28f6b61
Vikram <vikky5vikram@gmail.com>
2024-03-24 00:11:06 +0530

    Committing code for OrderItemRepository

--- src/main/java/com/vikram/repository/NotificationRepository.java
+++ src/main/java/com/vikram/repository/NotificationRepository.java
@@ -3,6 +3,8 @@ package com.vikram.repository;
 import com.vikram.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     public List<Notification> findByCustomerId(Long userId);
---
+++ src/main/java/com/vikram/repository/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.vikram.repository;
+
+import com.vikram.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    
+
+}
