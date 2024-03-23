fc52ba962d2b87cdeda3429a025795a5c368fe45
Vikram <vikky5vikram@gmail.com>
2024-03-24 02:04:44 +0530

    Committing code for AuthResponse

---
+++ src/main/java/com/vikram/response/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.vikram.response;
+
+import com.vikram.domain.USER_ROLE;
+import lombok.Data;
+
+
+@Data
+public class AuthResponse {
+
+    private String message;
+    private String jwt;
+    private USER_ROLE role;
+
+}
