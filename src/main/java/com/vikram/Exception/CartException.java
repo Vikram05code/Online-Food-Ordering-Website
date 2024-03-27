12ba36d4a42575a6a58e167f054bea8433b279da
Vikram <vikky5vikram@gmail.com>
2024-03-27 15:19:29 +0530

    Committing code for CartException

---
+++ src/main/java/com/vikram/Exception/CartException.java
@@ -0,0 +1,9 @@
+package com.vikram.Exception;
+
+public class CartException extends Exception {
+
+    public CartException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/com/vikram/Exception/FoodException.java
+++ src/main/java/com/vikram/Exception/FoodException.java
@@ -1,7 +1,7 @@
 package com.vikram.Exception;
 
 public class FoodException extends Exception{
-    
+
     public FoodException(String msgs){
         super(msgs);
     }
