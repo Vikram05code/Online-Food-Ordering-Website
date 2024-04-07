072c4f0dccf0ea1d36f27a3db73d6c5aabe99c37
Vikram <vikky5vikram@gmail.com>
2024-04-07 14:16:36 +0530

    Committing code for RestaurantController handler Method findRestaurantByName

---
+++ src/main/java/com/vikram/controller/RestaurantController.java
@@ -0,0 +1,37 @@
+package com.vikram.controller;
+
+import com.vikram.model.Restaurant;
+import com.vikram.service.RestaurantService;
+import com.vikram.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/restaurants")
+public class RestaurantController {
+
+    @Autowired
+   private RestaurantService restaurantService;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Restaurant>> findRestaurantByName(@RequestParam String keyword){
+        List<Restaurant> restaurants = restaurantService.searchRestaurant(keyword);
+
+        return new ResponseEntity<>(restaurants, HttpStatus.OK);
+    }
+
+    
+
+
+}
