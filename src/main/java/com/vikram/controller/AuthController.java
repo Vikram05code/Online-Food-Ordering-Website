a09f5e42db8d48cb73b1aa47ab9e21c8acc88702
Vikram <vikky5vikram@gmail.com>
2024-04-08 08:40:14 +0530

    Committing code for AuthController requestHandler method signin

--- src/main/java/com/vikram/controller/AuthController.java
+++ src/main/java/com/vikram/controller/AuthController.java
@@ -7,16 +7,20 @@ import com.vikram.model.Cart;
 import com.vikram.model.User;
 import com.vikram.repository.CartRepository;
 import com.vikram.repository.UserRepository;
+import com.vikram.request.LoginRequest;
 import com.vikram.response.AuthResponse;
+import com.vikram.service.CustomUserDetailsService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -41,6 +46,8 @@ public class AuthController {
 
     @Autowired
     private JwtProvider jwtProvider;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
 
     @PostMapping("/signup")
@@ -79,10 +86,48 @@ public class AuthController {
 
         AuthResponse authResponse = new AuthResponse();
         authResponse.setJwt(token);
-        authResponse.setMessage("Registeration Successful");
+        authResponse.setMessage("Registration Successful");
         authResponse.setRole(savedUser.getRole());
 
         return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signIn(@RequestBody LoginRequest loginRequest){
+
+        String userName = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        Authentication authentication = authenticate(userName, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse();
+
+        authResponse.setMessage("Login Successful");
+        authResponse.setJwt(token);
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        String roleName = authorities.isEmpty()?null:authorities.iterator().next().getAuthority();
+
+        authResponse.setRole(USER_ROLE.valueOf(roleName));
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String userName, String password) {
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(userName);
+
+        if(userDetails==null){
+            throw new BadCredentialsException("Invalid UserName or Password");
+        }
+        if(passwordEncoder.matches(password, userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid UserName or Password");
+        }
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities());
+
+        return authentication;
     }
 
 
