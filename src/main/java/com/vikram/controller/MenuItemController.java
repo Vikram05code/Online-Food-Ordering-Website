f6a4b7a14a77ff20946784209b714bde05c61d59
Vikram <vikky5vikram@gmail.com>
2024-04-18 08:55:18 +0530

    Committing code for ManuItemController

---
+++ src/main/java/com/vikram/controller/MenuItemController.java
@@ -0,0 +1,44 @@
+package com.vikram.controller;
+
+
+import com.vikram.Exception.FoodException;
+import com.vikram.model.Food;
+import com.vikram.service.FoodService;
+import com.vikram.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/food")
+public class MenuItemController {
+    @Autowired
+    private FoodService menuItemService;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Food>> searchFood(
+            @RequestParam String name)  {
+        List<Food> menuItem = menuItemService.searchFood(name);
+        return ResponseEntity.ok(menuItem);
+    }
+    @GetMapping("/restaurant/{restaurantId}")
+    public ResponseEntity<List<Food>> getMenuItemByRestaurantId(
+            @PathVariable Long restaurantId,
+            @RequestParam boolean vegetarian,
+            @RequestParam boolean seasonal,
+            @RequestParam boolean nonveg,
+            @RequestParam(required = false) String food_category) throws FoodException {
+        List<Food> menuItems= menuItemService.getRestaurantsFood(
+                restaurantId,vegetarian,nonveg,seasonal,food_category);
+        return ResponseEntity.ok(menuItems);
+    }
+
+
+
+}
